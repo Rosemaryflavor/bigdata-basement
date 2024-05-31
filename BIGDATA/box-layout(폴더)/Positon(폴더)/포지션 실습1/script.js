4c34fd90c2ee45bc080deec6f6efe0997785d371
Rosemaryflavor <weesy44@naver.com>
2024-05-31 11:18:04 +0900

    🎉 0531 맥도날드 만들어보란다

--- BIGDATA/box-layout(폴더)/Positon(폴더)/포지션 실습1/script.js
+++ BIGDATA/box-layout(폴더)/Positon(폴더)/포지션 실습1/script.js
@@ -8,3 +8,28 @@ popupOpenBtn.addEventListener('click', function(){
 popupCloseBtn.addEventListener('click', function(){
   popup.classList.remove('is-active');
 })
+
+let sidebar = document.querySelector('.sidebar')
+let sidebarOpenBtn = document.querySelector('.sidebar-open-btn')
+let sidebarCloseBtn = document.querySelector('.sidebar-close-btn')
+
+sidebarOpenBtn.addEventListener('click', function(){
+  sidebar.classList.add('is-active');
+})
+sidebarCloseBtn.addEventListener('click', function(){
+  sidebar.classList.remove('is-active');
+})
+
+// function openSidebar(){
+//   sidebar.classList.add('is-active');
+// }
+// function CloseSidebar(){
+//   sidebar.classList.remove('is-active');
+// }
+// sidebarOpenBtn.addEventListener('click', openSidebar);
+// sidebarCloseBtn.addEventListener('click', CloseSidebar);
+
+// function plus (num){
+//   console.log(2124 + num);
+// }
+// plus(10);
