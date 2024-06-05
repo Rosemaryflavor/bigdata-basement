384d77003ec82276b213dc883e72a487de71f7d8
Rosemaryflavor <weesy44@naver.com>
2024-06-05 09:38:19 +0900

    🤡 이과 정보의 홍수

--- BIGDATA/JS-basic/html요소선택/script.js
+++ BIGDATA/JS-basic/html요소선택/script.js
@@ -19,3 +19,25 @@ document.querySelectorAll('.item')[1].style.color = 'blue';
 document.querySelectorAll('.item')[2].style.color = 'blue';
 // J-Query 코드
 $('.item').css('color','red');
+
+// items.forEach((item) => {
+//   item.innerHTML = '반복문';
+// })
+
+// for(let i = 0; i <= items.length; i++) {
+//   items[i].innerHTML = 'for 반복문';
+ 
+// }
+
+let num = 5;
+let num1 = '5';
+let txt = '안녕하세요'
+console.log(typeof(num1))
+console.log(num1)
+console.log((txt));
+console.log((typeof(txt)));
+
+// Array(배열), Object: 여러 자료를 한번에 담을 때 씀 
+// Array: []  Object: {}
+// let arr = ['may', 3543, 'August' '니하오']
+// console.log(arr[0])
