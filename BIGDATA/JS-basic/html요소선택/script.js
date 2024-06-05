6a54789ec84bb567906c9034eb480906cfbd9d9c
Rosemaryflavor <weesy44@naver.com>
2024-06-05 10:34:47 +0900

    🎉 0605연산자

--- BIGDATA/JS-basic/html요소선택/script.js
+++ BIGDATA/JS-basic/html요소선택/script.js
@@ -39,5 +39,26 @@ console.log((typeof(txt)));
 
 // Array(배열), Object: 여러 자료를 한번에 담을 때 씀 
 // Array: []  Object: {}
-// let arr = ['may', 3543, 'August' '니하오']
-// console.log(arr[0])
+let arr = ['may', 3543, 'August', 7754];
+console.log(arr[1])
+
+// let obj = {
+//   이름 : 'seoyeon'
+//   종족 : '인간'
+// }
+// console.log(obj)
+
+let hi = '안녕하세요 저는';
+let name = '위서연';
+let my = hi + name + '입니다';
+let my2 = `안녕하세요 저는 ${name} 입니다`
+let my1 = `안녕하세요 저는 입니다`;
+console.log(my1)
+
+// 매개변수(parameter)로 이름값
+function info(myName){
+  console.log('안녕하세요 저는 ${myName} 입니다');
+}
+
+info('아무개');
+
