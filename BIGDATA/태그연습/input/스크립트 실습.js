a6c5d863ed266aa5f1baede6b23218c943a576bc
Rosemaryflavor <weesy44@naver.com>
2024-06-10 12:38:12 +0900

    🤡 길을 완전히 잃었다

--- BIGDATA/태그연습/input/스크립트 실습.js
+++ BIGDATA/태그연습/input/스크립트 실습.js
@@ -1,15 +1,28 @@
+let a, b, c = true;
+
 let userid = document.querySelector.getElementedById('userid');
 let idWarn = document.querySelector('.id-warn');
+
+userid.addEventListener('focusout', fuction()) ;{
+  if(userid.value.length < 8) ;{
+    idWarn.innerHTML = `<span class="txt-red">아이디를 8글자 이상 입력하시오.</span>`;
+  } {
+    idWarn.innerHTML = `<span class="txt-green">멋진 아이디입니다!</span>`;
+  }
+}
 
 let userpw = document.getElementById('userpw');
 let pwWarn = document.querySelector('.pw-warn');
 
-userid.addEventListener('focusout', fuction()) ;{
-  if(userid.Value.length < 8) ;{
-    idWarn.innerHTML = `<span class="txt-red">아이디를 8글자 이상 입력하시오.</span>`
-  }
-}
+userpw.addEventListener('focusout', function() {
+  let pwExp = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,16}$/;
 
+  if(!pwExp.test(userpw.value)) {
+    pwWarn.innerHTML = `<span class="txt-red">8~16자 영문 대문자, 소문자, 숫자를 사용하시오.</span>`
+  } else {
+    pwWarn.innerHTML = '';
+  }
+})
 
 let year = document.querySelector.getElementedById('year');
 let month = document.querySelector.getElementedById('month');
@@ -21,3 +34,11 @@ let getYear = kst.getFullYear();
 for(let i = 1920; i <= getYear; i++){
   let  option = `<option>${i}</option>`
 }
+
+let checkBoxs = document.querySelectorAll('input[type="checkbox"][name="hobby"]');
+
+checkBoxs.forEach((item) => { 
+  item.addEventListener('change', () => {
+    let checkCount = document.querySelectorAll('input[type="checkbox"][name="hobby"]').length;
+  })
+})
